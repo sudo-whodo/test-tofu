a735ed7f33434f2bb87b19b46f8a745bf8fa289d
frazz <59382025+Sudo-Whodo@users.noreply.github.com>
2025-10-30 16:46:30 -0700

    updating workflow

--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Name of the S3 bucket to create"
   type        = string
+  default     = "my-tofu-created-bucket-12345"
 }
 
 variable "environment" {
